d5810ef3a4bdc72f0cb7b34eb1f9a4eaaf449aa5
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-10-17 13:17:10 -0400

    Settin up

---
+++ CookingRecipies/Component/RecipeCards.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import {View} from "react-native";
+
+const RecipeCards = () => {
+    let [state, setState] = useState("")
+    return(
+        <View>
+
+        </View>
+
+    )
+}
