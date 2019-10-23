28093ab8e9e8b0a0ab17f93dcba67f3fca40a8b9
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-10-23 15:22:21 -0400

    merged with navigation

---
+++ CookingRecipies/Component/MyCookBook.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from "react";
+import 
+{View,TouchableOpacity, TextInput, Button, StyleSheet, Text} 
+from "react-native";
+
+
+const MyCookBook = (props) =>{
+    const[folderName, setFolderName] = useState([
+        {
+            course: '',
+            recipes:  []
+        }
+    ]);
+    const [savedRecipes,  setSavedRecipes] = useState([props]);
+
+    useEffect(()=>{
+        for(i in savedRecipes){
+            for(x in savedRecipses){
+                if(i.course == folderName.course){
+                    setFolderName(...folderName, {...folderName.course, recpies: recpies.append(i)})
+                }
+                setFolderName(...folderName, {course: i.course, recpies: recpies.append(i)})
+            }
+        }
+
+    },[]);
+
+    return(
+        <View>
+            {/* <Text>{user.props.name}</Text> */}
+            {folderName.props.map(folder  => {
+                return (
+                    <TouchableOpacity style={styles.button}>
+                    <Button  
+                    color="white"    
+                    onPress={()  =>  this.props.navigation('CookBookFolder',  {data: folder})}
+                    title={folder.courses}
+                    accessibilityLabel="Search"                   
+                    />
+                </TouchableOpacity>
+                )
+            })}
+        </View>
+    )
+
+}
