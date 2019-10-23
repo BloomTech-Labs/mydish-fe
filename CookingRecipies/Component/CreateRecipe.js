6d947bab52af46ab062052be8cee57466ccc18f6
Kenya-a <Kenya-a@users.noreply.github.com>
2019-10-23 14:11:00 -0500

    added new form and new dependency for dropdown

--- CookingRecipies/Component/CreateRecipe.js
+++ CookingRecipies/Component/CreateRecipe.js
@@ -1,89 +1,83 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
-
-import t from 'tcomb-form-native';
-
-const Form = t.form.Form;
-
-const User = t.struct({
-  dish: t.String,
-  ingredients: t.String,
-  directions: t.String,
-  terms: t.Boolean
-});
-
-const formStyles = {
-  ...Form.stylesheet,
-  formGroup: {
-    normal: {
-      marginBottom: 10
-    },
-  },
-  controlLabel: {
-    normal: {
-      color: 'blue',
-      fontSize: 18,
-      marginBottom: 7,
-      fontWeight: '600'
-    },
-    // the style applied when a validation error occours
-    error: {
-      color: 'red',
-      fontSize: 18,
-      marginBottom: 7,
-      fontWeight: '600'
-    }
-  }
-}
-
-const options = {
-  fields: {
-    dish: {
-      error: 'Name your Recipe',
-      label: 'Recipe Name'
-    },
-    ingredients: {
-      error: 'Add ingredients so others know what they should have.'
-    },
-    directions: {
-      error: 'Add instructions for others to follow.'
-    },
-    terms: {
-      label: 'Save to CookBook',
-    },
-  },
-  stylesheet: formStyles,
-};
+import { Text, TextInput, View, Image, StyleSheet,} from 'react-native';
 
 export default class CreateRecipe extends Component {
-  handleSubmit = () => {
-    const value = this._form.getValue();
-    console.log('value: ', value);
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      value: ''
+
+    };
   }
-  
+
   render() {
     return (
-      <View style={styles.container}>
-        <Form 
-          ref={c => this._form = c}
-          type={User} 
-          options={options}
+      <View style={{padding: 40}}>
+        <Text style = {styles.header}>Create Recipe</Text>
+        <View style = {{flexDirection: "row", padding: 15}}>
+          
+        <Image
+          style={{width: 150, height: 150}}
+          resizeMode = 'contain'
+          source={{uri : 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2018/05/vegan-chilli.jpg'}}
         />
-        <Button
-          title="Submit Recipe"
-          onPress={this.handleSubmit}
+
+        <View>
+        <Text style = {styles.titleText} >Honey Pancakes</Text>
+          <Text style = {styles.baseText}>Recipe by: Lana Smith 
+          {/* {this.state.username} */}
+          </Text>
+        </View>
+          
+
+        </View>
+        
+        <Text>Recipe Name</Text>
+        <TextInput 
+        multiline = {true}
+        numberOfLines = {5}
+        maxLength = {55}
+        placeholder='Just a placeholder for Recipe Name'
+        value = {this.state.value}
+        onChangeText = {(value) => this.setState({value})}/>
+        <Text style= {{alignSelf:'flex-end'}}>
+          {this.state.value.length}/55
+        </Text>
+
+        <TextInput
+          style={{height: 40}}
+          // label = "Recipe Name"
+          placeholder="Just a placeholder"
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
         />
+        <TextInput
+          style={{height: 40}}
+          placeholder="Cuisine"
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+
+    
       </View>
     );
   }
 }
 
-
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    marginTop: 50,
-    padding: 20,
-    backgroundColor: '#ffffff',
+  baseText: {
+    fontSize:15,
+  },
+  header: {
+    fontSize:30,
+    fontWeight:'bold'
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
