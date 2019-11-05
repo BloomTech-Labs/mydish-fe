6d4ff90eef4867a5626c13edd036cfbdb4a3be50
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-11-05 15:10:04 -0500

    merging with create recipe styles for the most up to date code

--- CookingRecipies/Components/signUp.js
+++ CookingRecipies/Components/signUp.js
@@ -15,6 +15,7 @@ import styles from '../styles/signUpStyles.js'
 
 const SignUp = props => {
     const [signUp, SetSignUp] = useState({username: '', password: ''})
+    const [err, setErr] = useState()
 
     console.log(signUp)
 
@@ -27,7 +28,7 @@ const SignUp = props => {
      console.log("axios call goes here")
       axios.post('https://recipeshare-development.herokuapp.com/cooks/register', signUp)
       .then(res => console.log('response from sign up axios post', res))
-      .catch(err => console.log('error from sign up axios post',err))
+      .catch(err => setErr(err))
     }
     
       return (
@@ -45,6 +46,7 @@ const SignUp = props => {
            style={styles.inputFeilds}
            value={signUp.username}
            onChangeText={event => SetSignUp({...signUp, username:event})}/>
+           {err !=null && <Text>{err}</Text>}
            <Text style={styles.passwordText}>Password</Text>
           <TextInput
            style={styles.inputFeilds}
