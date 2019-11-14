27d12f3b4da398686bfce48521e6bd385b516be4
Amir Yunas <ayunas@gmail.com>
2019-11-14 16:45:10 -0600

    fixed filenaming issues

--- CookingRecipesExpo/navigation/RecipeNavigator.js
+++ CookingRecipesExpo/navigation/RecipeNavigator.js
@@ -2,7 +2,6 @@ import {createStackNavigator} from 'react-navigation-stack';
 import HomePage from '../components/HomePage';
 import IndividualRecipe from '../components/IndividualRecipe';
 
-
 const RecipeNavigator = createStackNavigator({
     Home : {screen : HomePage},
     IndividualR : {screen : IndividualRecipe}
