353d9b1d2212ee6242e5ebc5a1bc7c8196a002c9
Amir Yunas <ayunas@gmail.com>
2019-11-17 23:49:22 -0600

    removing CookingRecipies react-native cli from github

--- CookingRecipies/__tests__/signup.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import signUp from '../Components/signUp';
-
-test.skip('signup snapshot', () => {
-    const signupSnap = renderer.create(<signUp/>).toJSON();
-    expect(signupSnap).toMatchSnapshot();
-})
-
