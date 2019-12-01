ae5d9210e10b6f2aaeea444b4eeae90320f9ab17
Amir Yunas <ayunas@gmail.com>
2019-12-01 08:59:53 -0600

    added auth.js to .gitignore

--- CookingRecipesExpo/components/ImageUpload.js
+++ CookingRecipesExpo/components/ImageUpload.js
@@ -1,33 +1,39 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, Image} from 'react-native';
 // import AWS from 'aws-sdk';
 // import { SOME_KEY } from 'react-native-dotenv';
 // const access_key = 'AKIA35GJTLYLRXJ3V2NC'
 // const secret = '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'
-import {access_key, secret_key, identityPoolId, region, bucket} from '../auth';
+import {identityPoolId, region, bucket} from '../auth';
 import image from '../assets/background.png';
-import Amplify from 'aws-amplify';
+// const image = require('../assets/background.png');
+import Amplify, {Storage} from 'aws-amplify';
 
 Amplify.configure({
     Auth: {identityPoolId,region},
     Storage : {bucket,region}
 })
 
-const {Storage} = Amplify;
-
 const upload = () => {
-    Storage.put('test.txt','Hello World')
+    Storage.put('logo.jpg', image, {contentType: 'image/jpeg'})
         .then(result => console.log('result from successful upload: ', result))
         .catch(err => console.log('error uploading to s3:', err));
 }
 
+const get = () => {
+    Storage.get('amir.txt')
+        .then(res => console.log('result get', res))
+        .catch(err => console.log('err getting', err))
+}
 
 function ImageUpload(props) {
 
     return (
-        <View>
-            <Text>text chosen automatically.  Click button to upload to S3</Text>
+        <View style={{alignItems : 'center'}}>
+            <Image style={{width: 100, height: 100}} source={image} />
+            <Text>Click button to upload above image to S3</Text>
             <Button title="Upload to S3" onPress={upload}/>
+            <Button title="Get from S3" onPress={get}/>
         </View>
     )
 
