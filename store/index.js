7b73695b0d25e1ee040fed0e811e7a5e82c49b92
Devin Warrick <dev.warr@gmail.com>
2019-12-16 11:00:12 -0600

    Add recipeReducer to the store

--- store/index.js
+++ store/index.js
@@ -2,7 +2,8 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 
 import { authReducer as auth } from "./auth/authReducer";
+import { recipeReducer as allRecipes } from "./auth/recipeReducer";
 
-const rootReducer = combineReducers({ auth });
+const rootReducer = combineReducers({ auth, allRecipes });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
