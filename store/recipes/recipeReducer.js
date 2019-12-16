37a6ad07603a79884d9df3881ceb94de1c976fe7
Devin Warrick <dev.warr@gmail.com>
2019-12-16 10:49:12 -0600

    Create basic reducer. Only has default case.

---
+++ store/recipes/recipeReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+    recipeList: [],
+    isLoading: false,
+    error: null
+}
+
+export const recipeReducer = (state = initState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
