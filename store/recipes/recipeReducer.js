93b7671627df85c4c2783cdfdc1b2682f85edfc6
Devin Warrick <dev.warr@gmail.com>
2019-12-16 10:59:25 -0600

    add fetchRecipes action to the recipeReducer

--- store/recipes/recipeReducer.js
+++ store/recipes/recipeReducer.js
@@ -1,12 +1,37 @@
+import {
+    START_FETCH_RECIPES,
+    FETCH_RECIPES_SUCCESS,
+    FETCH_RECIPES_FAILURE,
+} from "./recipeActions";
+
 const initState = {
     recipeList: [],
     isLoading: false,
-    error: null
-}
+    error: null,
+};
 
 export const recipeReducer = (state = initState, action) => {
     switch (action.type) {
+        case START_FETCH_RECIPES:
+            return {
+                ...state,
+                error: null,
+                isLoading: true,
+            };
+        case FETCH_RECIPES_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                recipeList: action.payload,
+            };
+        case FETCH_RECIPES_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload,
+            };
+
         default:
             return state;
     }
-}
+};
