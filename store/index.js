fe1ee87a1529119632b68486566e43b33ffd7cdd
Devin Warrick <dev.warr@gmail.com>
2019-12-16 20:57:32 -0600

    Add cookbook to redux store

--- store/index.js
+++ store/index.js
@@ -3,7 +3,8 @@ import thunk from "redux-thunk";
 
 import { authReducer as auth } from "./auth/authReducer";
 import { recipeReducer as allRecipes } from "./recipes/recipeReducer";
+import { cookbookReducer as cookbook } from "./cookbook/cookbookReducer";
 
-const rootReducer = combineReducers({ auth, allRecipes });
+const rootReducer = combineReducers({ auth, allRecipes, cookbook });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
