2a2727d857238167c4ac69267021d19d3c31e4ec
Devin Warrick <dev.warr@gmail.com>
2019-12-16 18:19:48 -0600

    Alow user to fetch recipes from cookbook

--- store/cookbook/cookbookAction.js
+++ store/cookbook/cookbookAction.js
@@ -3,6 +3,15 @@ import axioswithAuth from "../../utils/axiosWithAuth";
 export const START_COOKBOOK_RECIPES = "START_COOKBOOK_RECIPES";
 export const COOKBOOK_RECIPES_SUCCESS = "COOKBOOK_RECIPES_SUCCESS";
 export const COOKBOOK_RECIPES_FAILURE = "COOKBOOK_RECIPES_FAILURE";
-export const fetchCookbook = () => {
-    
-}
+export const fetchCookbook = () => dispatch => {
+    dispatch({ type: START_COOKBOOK_RECIPES });
+
+    axioswithAuth()
+        .get("/cookbook/")
+        .then(res => {
+            dispatch({ type: COOKBOOK_RECIPES_SUCCESS, payload: res.data });
+        })
+        .catch(err => {
+            dispatch({ type: COOKBOOK_RECIPES_FAILURE, payload: err });
+        });
+};
--- store/cookbook/cookbookReducer.js
+++ store/cookbook/cookbookReducer.js
@@ -1,3 +1,9 @@
+import {
+    START_COOKBOOK_RECIPES,
+    COOKBOOK_RECIPES_SUCCESS,
+    COOKBOOK_RECIPES_FAILURE,
+} from "./cookbookAction";
+
 const initState = {
     cookbookRecipes: [],
     isLoading: false,
@@ -6,6 +12,25 @@ const initState = {
 
 export const cookbookReducer = (state = initState, action) => {
     switch (action.type) {
+        case START_COOKBOOK_RECIPES:
+            return {
+                ...state,
+                isLoading: true,
+                error: null,
+            };
+        case COOKBOOK_RECIPES_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                cookbookRecipes: action.payload,
+            };
+        case COOKBOOK_RECIPES_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload,
+            };
+
         default:
             return state;
     }
