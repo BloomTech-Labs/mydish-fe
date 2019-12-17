7936e43a53e3a81bcdeeb5b64b2a1abd09e32475
Devin Warrick <dev.warr@gmail.com>
2019-12-16 21:41:40 -0600

    Refactor RecipeList to connect with redux store ond solely render single recipes

--- components/RecipeList.js
+++ components/RecipeList.js
@@ -1,31 +1,21 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
 import Recipe from "./Recipe";
 import { ScrollView, View } from "react-native";
 import axiosWithAuth from "../utils/axiosWithAuth";
 import styles from "../styles/recipe-styles";
 import RecipeListContainer from "./StyledComponents/RecipeListContainer";
-import {
-    LeftHeightAdjustment,
-    RightHeightAdjustment,
-    LeftAdjustImageHeight,
-    RightAdjustImageHeight,
-} from "../utils/helperFunctions/recipeListStyleFunctions";
+import { fetchCookbook } from "../store/cookbook/cookbookAction";
 
 const RecipeList = props => {
-    const CancelToken = axios.CancelToken;
-    const source = CancelToken.source();
-
-    let recipeList = props.recipes;
-
-    const [recipes, setRecipes] = useState([]); //namespace collision with the recipes in <Search/>
-    let [cookbook, setCookbook] = useState([]);
-
-    const cookbookURL =
-        "https://recipeshare-development.herokuapp.com/cookbook/";
-
-    const likedByUser = cookbook => {
-        recipeList = recipeList.map(rec => {
+    const dispatch = useDispatch();
+    const recipeList = useSelector(store => store.allRecipes.recipeList);
+    const cookbook = useSelector(store => store.cookbook.cookbookRecipes);
+    
+    const [recipes, setRecipes] = useState([]);
+    
+    const likedByUser = () => {
+        newRecipeList = recipeList.map(rec => {
             const match = cookbook.find(({ id }) => id === rec.id);
             if (match) {
                 rec.likedByUser = true;
@@ -34,58 +24,27 @@ const RecipeList = props => {
             }
             return rec;
         });
-        setRecipes(recipeList);
-    };
-
-    const getCookbook = async () => {
-        try {
-            const axiosAuth = await axiosWithAuth();
-            const res = await axiosAuth.get(cookbookURL, {
-                cancelToken: source.token,
-            });
-            setCookbook(res.data);
-            likedByUser(res.data);
-        } catch (err) {
-            if (axios.isCancel(err)) console.log("We cancelled the list.");
-            setRecipes(recipeList);
-        }
+        setRecipes(newRecipeList);
     };
 
     useEffect(() => {
-        getCookbook();
-        return () => {
-            console.log("List closing");
-            source.cancel();
-        };
-    }, [props.recipes]);
+        if (!cookbook.length) {
+            dispatch(fetchCookbook);
+        }
+        likedByUser();
+    }, [cookbook, recipeList]);
 
+
+    // TODO: Talk with backend - If we can get each recipe to have an
+    //       extra property called "forkCount", we can pass the forkCount
+    //       down to the <Recipe/> component. This could help the user see
+    //       how many times a recipe has been forked '' '
     return (
         <ScrollView>
             <RecipeListContainer>
-                {recipes.map((recp, index) => (
-                    <Recipe
-                        key={recp.id}
-                        recipe={recp}
-                        recipeList={props.recipes}
-                        forks={props.forks}
-                        setRecipeList={props.setRecipes}
-                        // imageHeight={LeftAdjustImageHeight()}
-                        // cardHeight={LeftHeightAdjustment()}
-                        courseType={props.courseType}
-                    />
+                {recipes.map(recp => (
+                    <Recipe key={recp.id} recipe={recp} />
                 ))}
-
-                {/* <View style={{flexDirection: 'column', width: "39%", paddingBottom: "60%"}}> */}
-                {/* <View style={styles.recipeContainer}>
-                 {recipes.map( (recp, index) => index%2 ==1 && 
-                        <Recipe key={recp.id} 
-                                recipe={recp} recipeList={props.recipes} 
-                                setRecipeList={props.setRecipes} 
-                                imageHeight={RightAdjustImageHeight()} 
-                                cardHeight={RightHeightAdjustment()}
-                                courseType={props.courseType}
-                        />)}
-                </View> */}
             </RecipeListContainer>
         </ScrollView>
     );
