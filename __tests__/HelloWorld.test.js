070cb074830d0952bbb9f773cc94d9aa522614b0
Devin Warrick <dev.warr@gmail.com>
2019-12-17 18:30:20 -0600

    HelloWorld test passes

--- __tests__/HelloWorld.test.js
+++ __tests__/HelloWorld.test.js
@@ -1,17 +1,18 @@
 import React from "react";
-import HelloWorld from "../../HelloWorld";
+import HelloWorld from "../HelloWorld";
 import { render } from "react-native-testing-library";
 import renderer from "react-test-renderer";
 
 describe("HelloWorld", () => {
-    it.skip("snapshot hello world", () => {
+    it("snapshot hello world", () => {
         const helloSnap = renderer.create(<HelloWorld />).toJSON();
         expect(helloSnap).toMatchSnapshot();
     });
 
-    it.skip("renders the correct message", () => {
-        const { queryByText } = render(<HelloWorld />);
-        expect(queryByText("Hello World")).not.toBeNull();
+    it("renders the correct message", () => {
+        const { getByTestId } = render(<HelloWorld />);
+        const text = getByTestId("hw")
+        expect(text.props.children).toMatch(/Hello /i);
     });
 
     it("display correct passed in name", () => {
