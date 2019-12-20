1c8fee8568dee66f0a30b8856d853f861723050c
jambis <imjaamees@gmail.com>
2019-12-20 12:01:23 -0600

    Recovered axiosWithAuth

--- utils/axiosWithAuth.js
+++ utils/axiosWithAuth.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 import { AsyncStorage } from "react-native";
-import { URL } from "react-native-dotenv";
-// import AsyncStorage from '@react-native-community/async-storage'
 
-const checkToken = async () => {
-    return await AsyncStorage.getItem("userToken");
-};
-
-const axiosWithAuth = () => {
-    const baseURL = URL || "https://recipeshare-development.herokuapp.com/";
-    return axios.create({ baseURL, headers: { Authorization: checkToken() } });
+const axiosWithAuth = async () => {
+    const userToken = await AsyncStorage.getItem("userToken");
+    const baseURL = "https://recipeshare-development.herokuapp.com/";
+    return axios.create({ baseURL, headers: { Authorization: userToken } });
 };
 
 export default axiosWithAuth;
