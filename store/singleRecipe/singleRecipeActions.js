89b1a14280468089e06bab59bc9f5a180d6698d3
Devin Warrick <dev.warr@gmail.com>
2020-01-07 01:51:40 -0600

    all edit actions now use thunk and dispatch(stopEdit()), added comments

--- store/singleRecipe/singleRecipeActions.js
+++ store/singleRecipe/singleRecipeActions.js
@@ -1,30 +1,40 @@
 import axiosWithAuth from "../../utils/axiosWithAuth";
 
 export const STOP_EDIT = "STOP_EDIT";
-export const START_UPDATE_RECIPE = "START_UPDATE_RECIPE"
-export const UPDATE_RECIPE_SUCCESS = "UPDATE_RECIPE_SUCCESS"
-export const UPDATE_RECIPE_FAILURE = "UPDATE_RECIPE_FAILURE"
+export const START_UPDATE_RECIPE = "START_UPDATE_RECIPE";
+export const UPDATE_RECIPE_SUCCESS = "UPDATE_RECIPE_SUCCESS";
+export const UPDATE_RECIPE_FAILURE = "UPDATE_RECIPE_FAILURE";
+
 let calling = false;
 export const stopEdit = () => async (dispatch, getState) => {
-    dispatch({ type: STOP_EDIT });
-    
-    // We call this function a few times in our components to make sure we stop editing.
-    // The calling variable makes sure that, if we're allready in this axios call, we won't 
-    // call multiple times until we're done with the current call.
+    dispatch({ type: STOP_EDIT }); // Stop editing our recipe
+
+    // We call this stopEdit() function a few times in our components to make sure we stop editing.
+    // The "calling" variable makes sure that, if we're allready in this axios call, we won't
+    // call the database multiple times until we're done with our current call '' '
     if (calling) return;
     calling = true;
     dispatch({ type: START_UPDATE_RECIPE });
 
+    const { recipe } = getState().singleRecipe; // grabbing the store from redux
     try {
-        const { recipe } = getState().singleRecipe;
         const axiosCustom = await axiosWithAuth();
         const res = await axiosCustom.put(`recipes`, recipe);
 
         dispatch({ type: UPDATE_RECIPE_SUCCESS, payload: res.data });
     } catch (err) {
-        console.log(err);
-        if (err.response.status > 400 && err.response.status < 500) {
-            dispatch({ type: UPDATE_RECIPE_FAILURE, payload: err, recipe });
+        const data = err.response.data;
+        // If we fail to update a recipe, there are many reasons why.
+        // One of the reasons could be that we're trying to update a recipe that isn't our own. (401, 403)
+        // If that's the case, the server returns a currentRecipe property so we can restore any
+        //      edited values back to their original.
+        // If we have a property called currentRecipe, we will use that to reset our store '' '
+        if (data.currentRecipe) {
+            dispatch({
+                type: UPDATE_RECIPE_FAILURE,
+                payload: err,
+                recipe: data.currentRecipe,
+            });
         }
         dispatch({ type: UPDATE_RECIPE_FAILURE, payload: err });
     } finally {
@@ -76,40 +86,49 @@ export const resetRecipe = () => {
     };
 };
 
+
+// When editing our individual recipe, if we ever stop editing 
+// - The title
+// - An ingredient
+// - An instruction
+// We want to make sure that we call our stopEdit() function in order to:
+// 1. return the STOP_EDIT action
+// 2. Call the database to update whatever edit we just made
+// Whenever we have "dispatch(stopEdit())",
+//     that's when we stop editing the recipe and call the database '' '
 export const EDIT_TITLE = "EDIT_TITLE";
-export const editTitle = value => {
-    if (value.charCodeAt(value.length - 1) === 10) {
-        return {
-            type: STOP_EDIT,
-        };
+export const editTitle = value => dispatch => {
+    if (value.charCodeAt(value.length - 1) === 10) dispatch(stopEdit());
+    else {
+        dispatch({
+            type: EDIT_TITLE,
+            payload: value,
+        });
     }
-    return {
-        type: EDIT_TITLE,
-        payload: value,
-    };
 };
 
 export const EDIT_INGRED = "EDIT_INGRED";
-export const editIngred = (index, value) => {
+export const editIngred = (index, value) => dispatch => {
     console.log(value);
-    return {
-        type: EDIT_INGRED,
-        payload: value,
-        index: index,
-    };
+    if (value.name.charCodeAt(value.length - 1) === 10) dispatch(stopEdit());
+    else {
+        dispatch({
+            type: EDIT_INGRED,
+            payload: value,
+            index: index,
+        });
+    }
 };
 
 export const EDIT_INSTRUCT = "EDIT_INSTRUCT";
-export const editInstruct = (index, value) => {
-    if (value.body.charCodeAt(value.body.length - 1) === 10) {
-        return {
-            type: STOP_EDIT,
-        };
+export const editInstruct = (index, value) => dispatch => {
+    if (value.body.charCodeAt(value.body.length - 1) === 10)
+        dispatch(stopEdit());
+    else {
+        dispatch({
+            type: EDIT_INSTRUCT,
+            payload: value,
+            index: index,
+        });
     }
-
-    return {
-        type: EDIT_INSTRUCT,
-        payload: value,
-        index: index,
-    };
 };
