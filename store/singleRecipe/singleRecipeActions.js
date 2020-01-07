7201ecd093f78684552f45b5e7ed7352af764549
Devin Warrick <dev.warr@gmail.com>
2020-01-07 01:19:48 -0600

    stopEdit function now calls database to update recipe; functions that used to return {type: STOP_EDIT} now call the stopEdit() function

--- store/singleRecipe/singleRecipeActions.js
+++ store/singleRecipe/singleRecipeActions.js
@@ -1,7 +1,36 @@
 import axiosWithAuth from "../../utils/axiosWithAuth";
 
 export const STOP_EDIT = "STOP_EDIT";
-export const stopEdit = () => ({ type: STOP_EDIT });
+export const START_UPDATE_RECIPE = "START_UPDATE_RECIPE"
+export const UPDATE_RECIPE_SUCCESS = "UPDATE_RECIPE_SUCCESS"
+export const UPDATE_RECIPE_FAILURE = "UPDATE_RECIPE_FAILURE"
+let calling = false;
+export const stopEdit = () => async (dispatch, getState) => {
+    dispatch({ type: STOP_EDIT });
+    
+    // We call this function a few times in our components to make sure we stop editing.
+    // The calling variable makes sure that, if we're allready in this axios call, we won't 
+    // call multiple times until we're done with the current call.
+    if (calling) return;
+    calling = true;
+    dispatch({ type: START_UPDATE_RECIPE });
+
+    try {
+        const { recipe } = getState().singleRecipe;
+        const axiosCustom = await axiosWithAuth();
+        const res = await axiosCustom.put(`recipes`, recipe);
+
+        dispatch({ type: UPDATE_RECIPE_SUCCESS, payload: res.data });
+    } catch (err) {
+        console.log(err);
+        if (err.response.status > 400 && err.response.status < 500) {
+            dispatch({ type: UPDATE_RECIPE_FAILURE, payload: err, recipe });
+        }
+        dispatch({ type: UPDATE_RECIPE_FAILURE, payload: err });
+    } finally {
+        calling = false;
+    }
+};
 
 export const START_EDIT = "START_EDIT";
 export const startEdit = () => ({ type: START_EDIT });
