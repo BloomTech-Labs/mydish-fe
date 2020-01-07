8ed80f0e86196524ad9719c17763afa4c94c4b07
olympia <owojcik7@gmail.com>
2020-01-07 12:55:02 -0500

    Add Icons and ability to swipe IndividualRecipeNotes

--- components/IndividualRecipeNotes.js
+++ components/IndividualRecipeNotes.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import { View, Text } from "react-native";
 import styles from "../styles/individualRecipeStyles";
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function IndividualRecipeNotes({ color, notes }) {
+
+    const swipeableEl = useRef(null);
+    const editHandler = () => {
+        // setEditing(true);
+        // dispatch(startEdit());
+        console.log('editing note')
+        swipeableEl.current.close();
+    };
+
     return (
         <>
             <View style={{ paddingRight: "80%" }}>
@@ -16,15 +27,42 @@ export default function IndividualRecipeNotes({ color, notes }) {
                     NOTES
                 </Text>
             </View>
-            <Text
-                style={
-                    color.active.includes("Ingredients")
-                        ? styles.hidden
-                        : styles.stepTextView
-                }
-            >
-                {notes}
-            </Text>
+            <View style={styles.swipeableContainer}>
+                <Swipeable
+                    ref={swipeableEl}
+                    renderRightActions={() => (
+                        <View style={styles.buttonContainer}>
+                            <View style={styles.editButton}>
+                                <FontAwesome
+                                    name="pencil-square-o"
+                                    size={20}
+                                    color="white"
+                                    style={styles.icon}
+                                    onPress={editHandler}
+                                />
+                            </View>
+                            <View style={styles.deleteButton}>
+                                <FontAwesome
+                                    name="trash-o"
+                                    size={20}
+                                    color="white"
+                                    style={styles.icon}
+                                    onPress={() => { }}
+                                />
+                            </View>
+                        </View>
+                    )}>
+                    <Text
+                        style={
+                            color.active.includes("Ingredients")
+                                ? styles.hidden
+                                : styles.stepTextView
+                        }
+                    >
+                        {notes}
+                    </Text>
+                </Swipeable>
+            </View>
         </>
     );
 }
