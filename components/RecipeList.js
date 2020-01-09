589bacf76e0398073fb746b97aca59ef60824393
Devin Warrick <dev.warr@gmail.com>
2020-01-09 16:19:02 -0600

    Added comments in RecipeList.js to explain changes

--- components/RecipeList.js
+++ components/RecipeList.js
@@ -8,11 +8,17 @@ import { fetchCookbook } from "../store/cookbook/cookbookAction";
 
 const RecipeList = ({parent, folder}) => {
     const dispatch = useDispatch();
+    // If the RecipeList is being rendered from the cookbook,
+    //     grab the props from that the cookbook is passing down.
+    // If the RecipeList ISN'T coming from the cookbook, then we 
+    //     useSelector to get all of the recipes.
     const recipeList = parent === "cookbook" ? folder : useSelector(store => store.allRecipes.recipeList);
     const cookbook = useSelector(store => store.cookbook.cookbookRecipes);
 
+    // Recipe.js does not use the "likedByUser" property,
+    //     but we'll keep this function for now because it is setting state
+    //     for the "recipes" variable
     const [recipes, setRecipes] = useState([]);
-
     const likedByUser = () => {
         newRecipeList = recipeList.map(rec => {
             const match = cookbook.find(({ id }) => id === rec.id);
@@ -27,6 +33,9 @@ const RecipeList = ({parent, folder}) => {
     };
 
     useEffect(() => {
+
+        // Only call this action if the recipe is NOT coming
+        //     from the cookbook
         if (!cookbook.length || parent !== "cookbook") {
             dispatch(fetchCookbook);
         }
