7e0f94739bf7f7b5f523fb2d20ba7c8b158ea783
kmcknight1 <katiemcknight1991@gmail.com>
2020-01-09 09:35:29 -0600

    connects AddNote to redux store

--- components/AddRecipeFields/AddNote.js
+++ components/AddRecipeFields/AddNote.js
@@ -6,9 +6,60 @@ import { addNote } from "../../store/singleRecipe/singleRecipeActions";
 import Add from "../Add";
 
 const AddNote = () => {
-  return (
-      <View><Add text="Add Note" /></View>
-  )
-}
+    const dispatch = useDispatch();
+    const [adding, setAdding] = useState(false);
+    const [note, setNote] = useState("");
+    const [highlighted, setHighlighted] = useState(false);
+
+    const cancelAdd = () => {
+        setAdding(false);
+        setHighlighted(false);
+    };
+
+    const submitAdd = () => {
+        if (!note.length) {
+            setHighlighted(true);
+        } else {
+            setAdding(false);
+            dispatch(addNote(note));
+        }
+    };
+
+    return (
+        <View>
+            {adding ? (
+                <View style={{ alignItems: "center" }}>
+                    <TextInput
+                        multiline
+                        returnKeyType="done"
+                        onSubmitEditing={submitAdd}
+                        style={{
+                            borderWidth: highlighted ? 1 : 0.8,
+                            borderColor: highlighted ? "#FF0000" : "#363838",
+                            borderRadius: 4,
+                            padding: 5,
+                            width: "90%",
+                            minHeight: 50,
+                            maxWidth: "90%",
+                        }}
+                        onChangeText={note => setNote(note)}
+                    />
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            width: "100%",
+                            justifyContent: "space-evenly",
+                        }}
+                    >
+                        <Button title="Cancel" onPress={cancelAdd} />
+                        <Button title="Submit" onPress={submitAdd} />
+                    </View>
+                </View>
+            ) : (
+                <Add text="Add Note" submit={() => setAdding(true)} />
+            )}
+        </View>
+    );
+};
 
 export default AddNote;
