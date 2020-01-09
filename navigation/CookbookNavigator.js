2cce81642dd14fd21f9e6124bb2c139c5b9807a7
olympia <owojcik7@gmail.com>
2020-01-09 13:27:10 -0500

    Add headerLayoutPreset to CookbookNavigator to center headerTitle on PersonalCookbook page

--- navigation/CookbookNavigator.js
+++ navigation/CookbookNavigator.js
@@ -9,7 +9,8 @@ const CookBookNavigator = createStackNavigator(
         Folder: { screen: CookBookFolder },
         IndividualR: { screen: IndividualRecipes },
     },
-    { initialRouteName: "CookBook" },
+    { initialRouteName: "CookBook",
+    headerLayoutPreset: 'center' },
 );
 
 export default CookBookNavigator;
