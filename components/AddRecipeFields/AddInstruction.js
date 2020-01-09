5b0cb60d08190c8d0d92488d1620a7e55c9dfa8a
kmcknight1 <katiemcknight1991@gmail.com>
2020-01-08 20:50:57 -0600

    adds cancel and submit buttons

--- components/AddRecipeFields/AddInstruction.js
+++ components/AddRecipeFields/AddInstruction.js
@@ -1,13 +1,41 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { View, TextInput, Button } from "react-native";
 
 import Add from "../Add";
 
 const AddInstruction = ({ color }) => {
     const [adding, setAdding] = useState(false);
+    const [instruction, setInstruction] = useState("");
+
+    const cancelAdd = () => {
+        setAdding(false);
+    };
+
+    const submitAdd = () => {};
 
     return (
         <View style={color.active !== "Instructions" && { display: "none" }}>
+            {adding && (
+                <View>
+                    <TextInput
+                        multiline
+                        style={{ borderWidth: 1, borderColor: "#363838" }}
+                        onChangeText={instruction =>
+                            setInstruction(instruction)
+                        }
+                    />
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            width: "90%",
+                            justifyContent: "space-evenly",
+                        }}
+                    >
+                        <Button title="Cancel" onPress={cancelAdd} />
+                        <Button title="Submit" onPress={submitAdd} />
+                    </View>
+                </View>
+            )}
             <Add text="Add Instruction" submit={() => setAdding(true)} />
         </View>
     );
