675f6129e4a22f40a0ddad4a4471c79dbd07853c
Devin Warrick <dev.warr@gmail.com>
2020-01-09 16:03:30 -0600

    Edits cookbook actions to take in a searchCategory

--- store/cookbook/cookbookAction.js
+++ store/cookbook/cookbookAction.js
@@ -3,12 +3,15 @@ import axiosWithAuth from "../../utils/axiosWithAuth";
 export const START_FETCH_COOKBOOK = "START_COOKBOOK_RECIPES";
 export const FETCH_COOKBOOK_SUCCESS = "FETCH_COOKBOOK_SUCCESS";
 export const FETCH_COOKBOOK_FAILURE = "FETCH_COOKBOOK_FAILURE";
-export const fetchCookbook = () => async dispatch => {
+export const fetchCookbook = searchCategory => async dispatch => {
     dispatch({ type: START_FETCH_COOKBOOK });
 
     try {
         const axiosCustom = await axiosWithAuth();
-        const res = await axiosCustom.get("cookbook/");
+        const url = searchCategory
+            ? `cookbook?category=${searchCategory}`
+            : "cookbook/";
+        const res = await axiosCustom.get(url);
 
         dispatch({ type: FETCH_COOKBOOK_SUCCESS, payload: res.data });
     } catch (err) {
