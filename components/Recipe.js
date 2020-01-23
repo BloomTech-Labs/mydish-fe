bd3ee86f9687a276a1915e0800550510a9d16373
Devin Warrick <dev.warr@gmail.com>
2020-01-23 11:31:14 -0600

    Changes keys in IndividualRecipe.js to the index; add parentheses when evaluating totalCookTime

--- components/Recipe.js
+++ components/Recipe.js
@@ -6,7 +6,7 @@ import placeholder from "../assets/recipe-image-placeholder.png";
 
 const Recipe = props => {
     const { navigation, recipe } = props;
-    const totalCookTime = recipe.prep_time || 0 + recipe.cook_time || 0
+    const totalCookTime = (recipe.prep_time || 0) + (recipe.cook_time || 0);
 
     return (
         <View style={styles.recipeContainer}>
