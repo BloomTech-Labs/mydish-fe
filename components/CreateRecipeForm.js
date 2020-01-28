90cc8ea4ff62ae7806fa4e60f497d7d32c51b828
jambis <imjaamees@gmail.com>
2020-01-28 16:32:38 -0600

    Added default author_comment when creating recipe

--- components/CreateRecipeForm.js
+++ components/CreateRecipeForm.js
@@ -62,6 +62,7 @@ function CreateRecipeForm(props) {
                 .map(
                     (note, i) => note.replace(/\n+/g, " "), // Remove any newlines
                 ),
+            author_comment: "Original Recipe",
         };
         console.log(postRecipe);
 
