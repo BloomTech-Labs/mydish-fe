a47b2447e3fe4c01932e44d2cff70dc303ca8327
Devin Warrick <dev.warr@gmail.com>
2020-01-29 00:45:32 -0600

    Removes unused DrawerNavigator. To see code, view commit 3bcaf6316e7cf499545dc10de9fc2f906c588814

--- navigation/AppContainer.js
+++ navigation/AppContainer.js
@@ -2,13 +2,11 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import MainNavigator from './MainNavigator';
 import AuthNavigator from './AuthNavigator';
 import AuthLoadingScreen from './AuthLoadingScreen';
-import DrawerNavigator from "./DrawerNavigator"
 
 const Navigation = createSwitchNavigator({
   AuthLoading: AuthLoadingScreen,
   App: MainNavigator,
   Auth: AuthNavigator,
-  Drawer: DrawerNavigator
 }, { initialRouteName: 'AuthLoading' })
 
 const AppContainer = createAppContainer(Navigation);
