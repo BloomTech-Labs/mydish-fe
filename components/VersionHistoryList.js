4ab202f4d0f9261f3fd646bc5f1f406109bfb40e
kmcknight1 <katiemcknight1991@gmail.com>
2020-02-04 15:34:27 -0600

    formats styling of author comment

--- components/VersionHistoryList.js
+++ components/VersionHistoryList.js
@@ -1,89 +1,93 @@
-import React, { useEffect } from "react"
-import { FlatList, Text, View, TouchableOpacity, StyleSheet } from "react-native"
-import { useSelector, useDispatch } from "react-redux"
+import React, { useEffect } from "react";
+import {
+    FlatList,
+    Text,
+    View,
+    TouchableOpacity,
+    StyleSheet,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 
-
-import { fetchAllVersionHistory } from "../store/version-control/versionControlActions"
-import formatdate from "../utils/helperFunctions/formatdate"
+import { fetchAllVersionHistory } from "../store/version-control/versionControlActions";
+import formatdate from "../utils/helperFunctions/formatdate";
 
 const VersionHistoryList = props => {
-    const versionList = useSelector(state => state.versionsList.versionsList)
-    const dispatch = useDispatch()
+    const versionList = useSelector(state => state.versionsList.versionsList);
+    const dispatch = useDispatch();
 
     const id = props.navigation.getParam("parentId", "parentId not passed");
 
     async function fetchAllVersions(id) {
-        await dispatch(fetchAllVersionHistory(id))
-
+        await dispatch(fetchAllVersionHistory(id));
     }
 
     useEffect(() => {
-        fetchAllVersions(id)
-
-    }, [id])
+        fetchAllVersions(id);
+    }, [id]);
 
     return (
-
         <View>
+            <FlatList
+                data={versionList}
+                keyExtractor={item => item.revision_number.toString()}
+                renderItem={({ item }) => {
+                    {
+                        /*Format date to Month, Day, Year, H:MM AM/MP */
+                    }
+                    formattedDate = formatdate(item.date_modified);
 
-            < FlatList data={versionList} keyExtractor={(item) => item.revision_number.toString()} renderItem={({ item }) => {
-
-                {/*Format date to Month, Day, Year, H:MM AM/MP */ }
-                formattedDate = formatdate(item.date_modified)
-
-                return (
-                    <TouchableOpacity
-                        onPress={() => {
-                            // getRecipeByRevisionNumber(item.changes.id, item.id)
-                            props.navigation.navigate("IndividualR", {
-                                recipeID: item.changes.id,
-                                revisionID: item.id,
-                            })
-                        }
-                        }>
-
-
-                        < View style={styles.versionView} >
-                            <Text style={styles.label}>Version {item.revision_number}. </Text>
-                            <Text>{formattedDate.toString()}</Text>
-                            <View style={{ flexDirection: 'row' }}>
-                                <Text style={styles.commentLabel}>Author Comment: </Text>
-                                <Text>{item.changes.author_comment}</Text>
+                    return (
+                        <TouchableOpacity
+                            onPress={() => {
+                                // getRecipeByRevisionNumber(item.changes.id, item.id)
+                                props.navigation.navigate("IndividualR", {
+                                    recipeID: item.changes.id,
+                                    revisionID: item.id,
+                                });
+                            }}
+                        >
+                            <View style={styles.versionView}>
+                                <Text style={styles.label}>
+                                    Version {item.revision_number}.{" "}
+                                </Text>
+                                <Text>{formattedDate.toString()}</Text>
+                                <View style={{ flexDirection: "row" }}>
+                                    <Text style={styles.commentLabel}>
+                                        Author Comment:{" "}
+                                    </Text>
+                                    <Text style={{ width: "65%" }}>
+                                        {item.changes.author_comment}
+                                    </Text>
+                                </View>
                             </View>
-                        </View >
-                    </TouchableOpacity >
-
-                )
-            }} />
-
+                        </TouchableOpacity>
+                    );
+                }}
+            />
         </View>
-
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     versionView: {
-        borderBottomColor: '#ccc',
+        borderBottomColor: "#ccc",
         borderBottomWidth: 1,
         margin: 15,
         paddingVertical: 4,
-        paddingHorizontal: 2
-    }
-    ,
+        paddingHorizontal: 2,
+    },
     label: {
         fontSize: 18,
         marginBottom: 15,
-        fontWeight: 'bold'
+        fontWeight: "bold",
     },
     commentLabel: {
-        fontWeight: 'bold'
-    }
-})
+        fontWeight: "bold",
+    },
+});
 
 VersionHistoryList.navigationOptions = {
-    headerTitle: "Version History"
-}
+    headerTitle: "Version History",
+};
 
-
-export default VersionHistoryList
-
+export default VersionHistoryList;
