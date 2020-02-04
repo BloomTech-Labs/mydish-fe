c039f58cc4e55e13b4c425e39ec215a731e12c3f
kmcknight1 <katiemcknight1991@gmail.com>
2020-02-04 15:33:46 -0600

    removes keyboard aware scrollview

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,19 +1,15 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import {
     View,
     Text,
     TextInput,
     TouchableOpacity,
-    KeyboardAvoidingView,
-    Image,
     SafeAreaView,
     ActivityIndicator,
 } from "react-native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { useDispatch, useSelector } from "react-redux";
 import { registerUser, clearError } from "../store/auth/authActions";
 import styles from "../styles/signUpStyles.js";
-import logo from "../assets/LogoGreen.png";
 import RecipeShareLogo from "./RecipeShareLogo.js";
 
 const SignUp = ({ navigation }) => {
@@ -34,72 +30,66 @@ const SignUp = ({ navigation }) => {
 
     return (
         <SafeAreaView>
-            <KeyboardAwareScrollView>
-                <View style={styles.container}>
-                    <Text style={styles.title}>Create Account</Text>
-                    <Text style={styles.explanationText}>
-                        Create a new account to save and edit your favorite
-                        recipes.
+            <View style={styles.container}>
+                <Text style={styles.title}>Create Account</Text>
+                <Text style={styles.explanationText}>
+                    Create a new account to save and edit your favorite recipes.
+                </Text>
+                <Text style={styles.emailText}>Username</Text>
+                <TextInput
+                    ref={usernameInput}
+                    style={styles.inputFields}
+                    value={signUp.username}
+                    returnKeyType="next"
+                    onSubmitEditing={() => passwordInput.current.focus()}
+                    onChangeText={event =>
+                        setSignUp({ ...signUp, username: event })
+                    }
+                />
+
+                <Text style={styles.passwordText}>Password</Text>
+                <TextInput
+                    ref={passwordInput}
+                    style={styles.inputFields}
+                    value={signUp.password}
+                    returnKeyType="done"
+                    onChangeText={event =>
+                        setSignUp({ ...signUp, password: event })
+                    }
+                    secureTextEntry={true}
+                />
+                {errorMsg != null && (
+                    <Text style={{ textAlign: "center", color: "red" }}>
+                        {errorMsg}
                     </Text>
-                    <Text style={styles.emailText}>Username</Text>
-                    <TextInput
-                        ref={usernameInput}
-                        style={styles.inputFields}
-                        value={signUp.username}
-                        returnKeyType="next"
-                        onSubmitEditing={() => passwordInput.current.focus()}
-                        onChangeText={event =>
-                            setSignUp({ ...signUp, username: event })
-                        }
-                    />
+                )}
 
-                    <Text style={styles.passwordText}>Password</Text>
-                    <TextInput
-                        ref={passwordInput}
-                        style={styles.inputFields}
-                        value={signUp.password}
-                        returnKeyType="done"
-                        onChangeText={event =>
-                            setSignUp({ ...signUp, password: event })
-                        }
-                        secureTextEntry={true}
-                    />
-                    {errorMsg != null && (
-                        <Text style={{ textAlign: "center", color: "red" }}>
-                            {errorMsg}
-                        </Text>
-                    )}
+                <TouchableOpacity
+                    onPress={() => {
+                        dispatch(clearError());
+                        navigation.navigate("Login");
+                    }}
+                >
+                    <Text style={styles.loginButton}>
+                        Have an account? Login
+                    </Text>
+                </TouchableOpacity>
 
+                <View style={{ flexDirection: "row-reverse" }}>
                     <TouchableOpacity
-                        onPress={() => {
-                            dispatch(clearError())
-                            navigation.navigate("Login");
-                        }}
+                        onPress={register}
+                        style={styles.createAccountButton}
                     >
-                        <Text style={styles.loginButton}>
-                            Have an account? Login
-                        </Text>
+                        {isLoading ? (
+                            <ActivityIndicator size="large" color="#00ff00" />
+                        ) : (
+                            <Text style={styles.createAccountText}>
+                                Create Account
+                            </Text>
+                        )}
                     </TouchableOpacity>
-
-                    <View style={{ flexDirection: "row-reverse" }}>
-                        <TouchableOpacity
-                            onPress={register}
-                            style={styles.createAccountButton}
-                        >
-                            {isLoading ? (
-                                <ActivityIndicator
-                                    size="large"
-                                    color="#00ff00"
-                                />
-                            ) : (
-                                <Text style={styles.createAccountText}>
-                                    Create Account
-                                </Text>
-                            )}
-                        </TouchableOpacity>
-                    </View>
                 </View>
-            </KeyboardAwareScrollView>
+            </View>
         </SafeAreaView>
     );
 };
