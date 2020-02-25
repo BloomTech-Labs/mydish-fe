f1f7ec54bf6889165f305ecefe360597f1aa482d
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-02-25 15:00:52 -0800

    adds RecipeImage component to CreateRecipeForm

---
+++ styles/recipeImageStyles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        flex: 1,
+        height: 200,
+        backgroundColor: "whitesmoke",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    image: {
+        height: "100%",
+        width: "100%",
+    },
+});
+
+module.exports = styles;
