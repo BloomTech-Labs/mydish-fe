67184b72e60389851a6c57a2969bfad5e9cf4e2a
kmcknight1 <katiemcknight1991@gmail.com>
2020-03-03 11:30:37 -0600

    displays ellipsis for username longer than maxUsername

--- components/IndividualRecipe.js
+++ components/IndividualRecipe.js
@@ -30,6 +30,7 @@ import styles from "../styles/individualRecipeStyles.js";
 import clock from "../assets/timer.png";
 import logo from "../assets/background.png";
 import placeholder from "../assets/recipe-image-placeholder.png";
+import { maxUsername } from "../constants/maxLenth";
 
 import Title from "./EditRecipeComponents/Title";
 import Tab from "./Tab";
@@ -65,7 +66,6 @@ function IndividualRecipe(props) {
     const currentActive = useSelector(
         state => state.singleRecipe.currentActive,
     );
-    const maxLengthUsername = 15;
 
     //Anytime someone navigations to here - it has ID, we could just also pass another value
     const id = props.navigation.getParam("recipeID", "params not passed");
@@ -487,7 +487,15 @@ function IndividualRecipe(props) {
                             </View>
                             <View style={{ flexDirection: "row" }}>
                                 <Image source={logo} style={styles.icon} />
-                                <Text>{recipe.owner.username}</Text>
+                                <Text>
+                                    {recipe.owner.username &&
+                                    recipe.owner.username.length > maxUsername
+                                        ? `${recipe.owner.username.slice(
+                                              0,
+                                              maxUsername,
+                                          )}...`
+                                        : recipe.owner.username}
+                                </Text>
                             </View>
 
                             <View style={styles.timeContainer}>
