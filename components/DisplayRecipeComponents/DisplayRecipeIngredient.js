9d1440382f957080879e7619acc4e561cfa2aea8
kmcknight1 <katiemcknight1991@gmail.com>
2020-03-06 09:02:03 -0600

    changes name to recipeFields because the value is used in more places than just ingredient

--- components/DisplayRecipeComponents/DisplayRecipeIngredient.js
+++ components/DisplayRecipeComponents/DisplayRecipeIngredient.js
@@ -5,8 +5,8 @@ import styles from "../../styles/individualRecipeStyles";
 
 const DisplayRecipeIngredient = ({ ingredient }) => {
     return (
-        <View style={styles.ingredientList}>
-            <Text style={styles.ingredientText}>
+        <View style={styles.recipeFieldsList}>
+            <Text style={styles.recipeFieldsText}>
                 {ingredient.quantity} {ingredient.units} {ingredient.name}
             </Text>
         </View>
