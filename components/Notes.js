189aae8f6f37862e1e0eadd8d028a23338dcb6e1
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-03-18 08:41:52 -0700

    deletes zombie code

--- components/Notes.js
+++ components/Notes.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, TextInput } from "react-native";
 import { useDispatch } from "react-redux";
 import {
     editNotes,
