ed1094c9110b2308800c158a0df8e3d426323a72
kmcknight1 <katiemcknight1991@gmail.com>
2020-03-19 23:35:55 -0500

    uses new icons, combines tab styles into one object

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { Image, AsyncStorage } from "react-native";
+import { Image } from "react-native";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import RecipeNavigator from "./RecipeNavigator";
 import CreateNavigator from "./CreateNavigator";
 import CookBookNavigator from "./CookbookNavigator";
-import Login from "../components/Login";
 import styles from "../styles/navigation.styles";
-import search from "../assets/Union.png";
-import plus from "../assets/add_circle_grey.png";
 import home from "../assets/home-icon.png";
-import logout from "../assets/account_circle.png";
-import fork from "../assets/restaurant_grey.png";
+import cookbook from "../assets/cookbook-icon.png";
+import create from "../assets/create-icon.png";
 
 const MainNavigator = createBottomTabNavigator(
     {
@@ -18,7 +15,7 @@ const MainNavigator = createBottomTabNavigator(
             screen: RecipeNavigator,
             navigationOptions: {
                 tabBarLabel: "Home",
-                tabBarIcon: <Image style={styles.homeTab} source={home} />,
+                tabBarIcon: <Image style={styles.tab} source={home} />,
                 tabBarOnPress: ({ navigation }) => {
                     navigation.navigate("Home");
                 },
@@ -28,7 +25,7 @@ const MainNavigator = createBottomTabNavigator(
             screen: CreateNavigator,
             navigationOptions: {
                 tabBarLabel: "Create",
-                tabBarIcon: <Image style={styles.createTab} source={plus} />,
+                tabBarIcon: <Image style={styles.tab} source={create} />,
                 tabBarOnPress: ({ navigation }) => {
                     navigation.navigate("Create");
                 },
@@ -38,24 +35,12 @@ const MainNavigator = createBottomTabNavigator(
             screen: CookBookNavigator,
             navigationOptions: {
                 tabBarLabel: "CookBook",
-                tabBarIcon: <Image style={styles.createTab} source={fork} />,
+                tabBarIcon: <Image style={styles.tab} source={cookbook} />,
                 tabBarOnPress: ({ navigation }) => {
                     navigation.navigate("CookBook");
                 },
             },
         },
-
-        // Profile: {
-        //     screen: Login,
-        //     navigationOptions: {
-        //         tabBarLabel: "Sign Out",
-        //         tabBarIcon: <Image style={styles.loginTab} source={logout} />,
-        //         tabBarOnPress: async ({ navigation }) => {
-        //             await AsyncStorage.clear();
-        //             navigation.navigate("Auth");
-        //         },
-        //     },
-        // },
     },
     {
         initialRouteName: "Home",
--- styles/navigation.styles.js
+++ styles/navigation.styles.js
@@ -1,8 +1,11 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    homeTab: { resizeMode: "contain", width: 25, height: 25, paddingLeft: 28 },
-    createTab: { width: 25, height: 25, paddingTop: 10 },
+    tab: {
+        resizeMode: "contain",
+        width: 25,
+        height: 25,
+    },
     loginTab: { width: 25, height: 25 },
     authHomeTab: { width: 20, height: 20, paddingLeft: 28 },
 });
