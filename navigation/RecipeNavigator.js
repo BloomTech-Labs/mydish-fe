3bd84c58425ed12c907e9c7543c86ff928f0267a
kmcknight1 <katiemcknight1991@gmail.com>
2020-03-23 02:06:29 -0500

    formats file

--- navigation/RecipeNavigator.js
+++ navigation/RecipeNavigator.js
@@ -1,17 +1,15 @@
 import { createStackNavigator } from "react-navigation-stack";
 import HomePage from "../components/HomePage";
 import IndividualRecipe from "../components/IndividualRecipe";
-import VersionHistoryList from "../components/VersionHistoryList"
+import VersionHistoryList from "../components/VersionHistoryList";
 
 const RecipeNavigator = createStackNavigator(
     {
         Home: { screen: HomePage },
         IndividualR: { screen: IndividualRecipe },
-        VersionHistoryList: { screen: VersionHistoryList }
+        VersionHistoryList: { screen: VersionHistoryList },
     },
     { initialRouteName: "Home", headerLayoutPreset: "center" },
 );
 
-
 export default RecipeNavigator;
-
