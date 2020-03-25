d08fa2216db05cf83070f4f1d1210e443522f746
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-03-25 09:12:24 -0700

    moves modal styles to separate stylesheet

--- components/EditRecipeComponents/Modal.js
+++ components/EditRecipeComponents/Modal.js
@@ -7,12 +7,12 @@ import {
     TextInput,
     TouchableHighlight,
     TouchableOpacity,
-    // Button,
 } from "react-native";
 //Analytics
 import { Analytics, Event } from "expo-analytics";
 const analytics = new Analytics("UA-159002245-1");
 import theme from "../../styles/theme.style";
+import styles from "../../styles/commitModalStyles";
 const CommitModal = props => {
     const { commitModal, setCommitModal, saveButtonEditedRecipe } = props;
     const [author_comment, setAuthor_comment] = useState();
@@ -39,36 +39,10 @@ const CommitModal = props => {
         <View style={{ flexDirection: "row" }}>
             <Modal visible={commitModal.save} animationType="fade" transparent>
                 <TouchableHighlight onPress={closeModalHandler}>
-                    <View
-                        style={{
-                            height: "100%",
-                            width: "100%",
-                            backgroundColor: "rgba(30, 27, 27, 0.45)",
-                            alignItems: "center",
-                            justifyContent: "center",
-                        }}
-                    >
+                    <View style={styles.modalContainer}>
                         <KeyboardAvoidingView behavior={"position"}>
-                            <View
-                                style={{
-                                    backgroundColor:
-                                        "rgba(300, 300, 300, 0.95)",
-                                    borderRadius: 10,
-                                    paddingVertical: 16,
-                                    paddingHorizontal: 24,
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                    marginHorizontal: 59,
-                                }}
-                            >
-                                <Text
-                                    style={{
-                                        textAlign: "left",
-                                        fontFamily: theme.BOLD_FONT_FAMILY,
-                                        fontSize: theme.REGULAR_FONT_SIZE,
-                                        paddingVertical: 2,
-                                    }}
-                                >
+                            <View style={styles.modal}>
+                                <Text style={styles.text}>
                                     What did you change on this version of the
                                     recipe?
                                 </Text>
@@ -79,32 +53,16 @@ const CommitModal = props => {
                                         setAuthor_comment(text)
                                     }
                                     style={{
-                                        fontFamily: theme.REGULAR_FONT_FAMILY,
-                                        padding: 8,
-                                        marginVertical: 16,
-                                        minHeight: 109,
-                                        width: "100%",
-                                        borderRadius: 5,
-                                        borderWidth: 1,
-                                        borderColor: highlighted
-                                            ? "red"
-                                            : "black",
+                                        ...styles.textInput,
+                                        ...(highlighted && styles.highlighted),
                                     }}
                                 />
-                                <View
-                                    style={{
-                                        flexDirection: "row",
-                                        justifyContent: "center",
-                                        width: "100%",
-                                    }}
-                                >
+                                <View style={styles.buttonContainer}>
                                     <TouchableOpacity
                                         onPress={closeModalHandler}
                                         style={{
-                                            ...theme.SECONDARY_BUTTON,
-                                            width: 100,
-                                            marginRight: 20,
-                                            marginBottom: 0,
+                                            ...styles.cancelButton,
+                                            ...styles.narrowButton,
                                         }}
                                     >
                                         <Text
@@ -117,8 +75,7 @@ const CommitModal = props => {
                                         onPress={saveModalHandler}
                                         style={{
                                             ...theme.PRIMARY_BUTTON,
-                                            width: 100,
-                                            marginBottom: 0,
+                                            ...styles.narrowButton,
                                         }}
                                     >
                                         <Text style={theme.PRIMARY_BUTTON_TEXT}>
