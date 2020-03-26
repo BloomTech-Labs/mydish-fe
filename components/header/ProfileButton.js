d0b3bc6a0f53d426cb7da011ef4a1f6f29bafa48
kmcknight1 <katiemcknight1991@gmail.com>
2020-03-26 15:21:17 -0500

    connects to profile action and store

--- components/header/ProfileButton.js
+++ components/header/ProfileButton.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Image, TouchableOpacity, Text } from "react-native";
+import { profilePageToggle } from "../../store/navigation/navigationActions";
 import { useSelector, useDispatch } from "react-redux";
-import profile from "../../assets/profile-icon.png";
+import profileIcon from "../../assets/profile-icon.png";
+import profileIconActive from "../../assets/profile-icon-red.png";
 
 export default function ProfileButton() {
+    const dispatch = useDispatch();
+    const profile = useSelector(state => state.navigation.profileOpen);
+
+    const togglePress = () => {
+        dispatch(profilePageToggle(!profile));
+    };
     return (
-        <TouchableOpacity>
-            <Image source={profile} />
+        <TouchableOpacity onPress={togglePress} style={{ marginLeft: 20 }}>
+            {profile ? (
+                <Image source={profileIconActive} />
+            ) : (
+                <Image source={profileIcon} />
+            )}
         </TouchableOpacity>
     );
 }
