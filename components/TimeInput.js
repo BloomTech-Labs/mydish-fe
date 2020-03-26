f6a97b44d08e0acb9d0faa566261e92ae64b69e4
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-03-26 12:49:23 -0700

    adds UI components, refactors to use props

--- components/TimeInput.js
+++ components/TimeInput.js
@@ -8,6 +8,54 @@ import {
 } from "../store/singleRecipe/singleRecipeActions";
 import styles from "../styles/createRecipeStyles";
 
-function TimeInput(props) {}
+function TimeInput({
+    type,
+    title,
+    errors,
+    highlighted,
+    setHighlighted,
+    recipe,
+    setRecipe,
+    recipeToRender,
+}) {
+    const dispatch = useDispatch();
+    return (
+        <View style={styles.timeContainer}>
+            <View style={styles.heading}>
+                <Text>{title}</Text>
+                {errors.includes("prep_time and/or cook_time") && (
+                    <Text style={styles.missingAsterisk}>*</Text>
+                )}
+            </View>
+            <TextInput
+                style={{
+                    ...styles.timeInputContainer,
+                    ...(highlighted[type] && styles.highlighted),
+                }}
+                placeholder="minutes"
+                keyboardType={"numeric"}
+                onChangeText={min => {
+                    if (min !== "" && (isNaN(Number(min)) || Number(min) === 0))
+                        return;
+                    if (savedRecipe) {
+                        if (type === "prep_time") {
+                            dispatch(editPreptime(min));
+                        } else if (type === "cook_time") {
+                            dispatch(editCooktime(min));
+                        }
+                    } else {
+                        setRecipe({
+                            ...recipe,
+                            [type]: min,
+                        });
+                    }
+                }}
+                value={recipeToRender[type] ? String(recipeToRender[type]) : ""}
+                onFocus={() => setHighlighted({ [type]: true })}
+                onBlur={() => setHighlighted({ [type]: false })}
+            />
+        </View>
+    );
+}
 
 export default TimeInput;
