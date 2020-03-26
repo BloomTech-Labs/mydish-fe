a469649404a96ca3485b769e40ded49e73c68aa7
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-03-26 12:39:03 -0700

    adds component with boilerplate

---
+++ components/TimeInput.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, TextInput } from "react-native";
+import { useDispatch } from "react-redux";
+
+import {
+    editPreptime,
+    editCooktime,
+} from "../store/singleRecipe/singleRecipeActions";
+import styles from "../styles/createRecipeStyles";
+
+function TimeInput(props) {}
+
+export default TimeInput;
