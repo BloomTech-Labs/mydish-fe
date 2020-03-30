b489d0976dc43b6d1bcf8277a7965a3baea79e87
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-03-30 13:34:41 -0700

    adds cases for starting fetch and failed fetch

--- store/version-control/versionControlActions.js
+++ store/version-control/versionControlActions.js
@@ -2,30 +2,32 @@ import axiosWithAuth from "../../utils/axiosWithAuth";
 
 export const START_FETCH_ALL_VERSION_HISTORY =
     "START_FETCH_ALL_VERSION_HISTORY";
+export const FETCH_ALL_VERSION_HISTORY_SUCCESS =
+    "FETCH_ALL_VERSION_HISTORY_SUCCESS";
+export const FETCH_ALL_VERSION_HISTORY_FAILURE =
+    "FETCH_ALL_VERSION_HISTORY_FAILURE";
 export const VERSION_BY_REVISION_NUM = "VERSION_BY_REVISION_NUM";
 
-export const fetchAllVersionHistory = id => {
-    return async dispatch => {
-        try {
-            const axiosCustom = await axiosWithAuth();
-            const res = await axiosCustom.get(`recipes/${id}/versions`);
-            const sortedVersionList = res.data.sort(function(a, b) {
-                a = a.revision_number;
+export const fetchAllVersionHistory = id => async dispatch => {
+    dispatch({ type: START_FETCH_ALL_VERSION_HISTORY });
+    try {
+        const axiosCustom = await axiosWithAuth();
+        const res = await axiosCustom.get(`recipes/${id}/versions`);
+        const sortedVersionList = res.data.sort(function(a, b) {
+            a = a.revision_number;
 
-                b = b.revision_number;
+            b = b.revision_number;
 
-                return a < b;
-            });
+            return a < b;
+        });
 
-            dispatch({
-                type: START_FETCH_ALL_VERSION_HISTORY,
-                versionsList: sortedVersionList,
-            });
-        } catch (error) {
-            console.log(error);
-            throw error;
-        }
-    };
+        dispatch({
+            type: FETCH_ALL_VERSION_HISTORY_SUCCESS,
+            versionsList: sortedVersionList,
+        });
+    } catch (error) {
+        dispatch({ type: FETCH_ALL_VERSION_HISTORY_FAILURE, payload: error });
+    }
 };
 
 export const RESET_ALL_VERSION_HISTORY = "RESET_ALL_VERSION_HISTORY";
--- store/version-control/versionControlReducer.js
+++ store/version-control/versionControlReducer.js
@@ -1,5 +1,7 @@
 import {
     START_FETCH_ALL_VERSION_HISTORY,
+    FETCH_ALL_VERSION_HISTORY_SUCCESS,
+    FETCH_ALL_VERSION_HISTORY_FAILURE,
     RESET_ALL_VERSION_HISTORY,
 } from "./versionControlActions";
 
@@ -13,7 +15,21 @@ export const versionControlRecucer = (state = initialState, action) => {
     switch (action.type) {
         case START_FETCH_ALL_VERSION_HISTORY:
             return {
+                ...state,
+                isFetching: true,
+                error: null,
+            };
+        case FETCH_ALL_VERSION_HISTORY_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
                 versionsList: action.versionsList,
+            };
+        case FETCH_ALL_VERSION_HISTORY_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload,
             };
         case RESET_ALL_VERSION_HISTORY:
             return initialState;
