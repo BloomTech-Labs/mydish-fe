6eb085fa4f000ff3d698386017adac76438c102e
kmcknight1 <katiemcknight1991@gmail.com>
2020-03-31 16:38:53 -0500

    creates ingredientPrediction actions and reducer

---
+++ store/ingredientPrediction/ingredientPredictionActions.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const START_FETCH_INGREDIENTS = "START_FETCH_INGREDIENTS";
+export const FETCH_INGREDIENTS_SUCCESS = "FETCH_INGREDIENTS_SUCCESS";
+export const FETCH_INGREDIENTS_FAILURE = "FETCH_INGREDIENTS_FAILURE";
+export const fetchIngredients = ingredients => dispatch => {
+    dispatch({ type: START_FETCH_INGREDIENTS });
+
+    axios
+        .post(
+            "http://mydish-ingredientprediction.eba-wmm2grnv.us-east-2.elasticbeanstalk.com/pred",
+            ingredients,
+        )
+        .then(res => {
+            dispatch({ type: FETCH_INGREDIENTS_SUCCESS, payload: res.data });
+        })
+        .catch(err => {
+            dispatch({ type: FETCH_INGREDIENTS_FAILURE, payload: err });
+        });
+};
