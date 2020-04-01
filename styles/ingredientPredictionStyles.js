7f07f7dfa5e52e659dda862b9f75a59c750e0e02
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-01 14:56:38 -0700

    adjust marginBottom of header

--- styles/ingredientPredictionStyles.js
+++ styles/ingredientPredictionStyles.js
@@ -5,7 +5,7 @@ module.exports = StyleSheet.create({
     headText: {
         color: theme.DARK_GREY_FONT_COLOR,
         marginTop: 20,
-        marginBottom: 5,
+        marginBottom: 16,
         fontSize: theme.REGULAR_FONT_SIZE,
         fontFamily: theme.REGULAR_FONT_FAMILY,
     },
