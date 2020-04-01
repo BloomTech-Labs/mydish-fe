3b418d6bb78e1684fb4abf19c77ae75860ec0c62
kmcknight1 <katiemcknight1991@gmail.com>
2020-04-01 16:04:50 -0500

    creates AddRecipeButton to be reusable throughout codebase

---
+++ components/AddRecipeButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+import styles from "../styles/recipe-styles";
+
+export default AddRecipeButton = ({ navigation }) => {
+    const pressHandler = () => navigation.navigate("Create");
+
+    return (
+        <TouchableOpacity onPress={pressHandler} style={styles.addRecipeButton}>
+            <Text style={styles.addRecipeButtonText}>Add recipe</Text>
+        </TouchableOpacity>
+    );
+};
