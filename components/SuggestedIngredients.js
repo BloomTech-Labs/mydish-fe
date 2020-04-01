21f8a77292a1a1def7835b9b6ca5f8892d15ce1a
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-01 09:54:10 -0700

    adds addIngredient function to SuggestedIngredients

--- components/SuggestedIngredients.js
+++ components/SuggestedIngredients.js
@@ -3,11 +3,31 @@ import { useSelector } from "react-redux";
 import { Text, View, TouchableOpacity } from "react-native";
 import styles from "../styles/ingredientPredictionStyles";
 
-const SuggestedIngredients = () => {
+const SuggestedIngredients = ({ recipe, setRecipe }) => {
     const suggestedIngredients = useSelector(
         state => state.ingredientPrediction.ingredients,
     );
-
+    const isEmpty = ingredient => {
+        return !ingredient.quantity && !ingredient.units && !ingredient.name;
+    };
+    const addIngredient = name => {
+        const ingredient = {
+            quantity: "",
+            units: "",
+            name: name,
+        };
+        const ingredients = recipe.ingredients;
+        const length = ingredients.length;
+        if (isEmpty(ingredients[length - 1])) {
+            ingredients.splice(length - 1, 1, ingredient);
+        } else {
+            ingredients.push(ingredient);
+        }
+        setRecipe(recipe => ({
+            ...recipe,
+            ingredients: ingredients,
+        }));
+    };
     return (
         <>
             {suggestedIngredients.length > 0 && (
@@ -16,7 +36,10 @@ const SuggestedIngredients = () => {
                     {suggestedIngredients.map((ing, i) => {
                         if (i < 5) {
                             return (
-                                <TouchableOpacity key={i}>
+                                <TouchableOpacity
+                                    key={i}
+                                    onPress={() => addIngredient(ing)}
+                                >
                                     <Text style={styles.ingredientText}>
                                         {ing}
                                     </Text>
