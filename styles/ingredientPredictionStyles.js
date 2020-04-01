4b58d73a8218e8805450e873d36ab95b474b790f
kmcknight1 <katiemcknight1991@gmail.com>
2020-03-31 21:01:56 -0500

    adds ingredient prediction stylesheet

---
+++ styles/ingredientPredictionStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+import theme from "./theme.style";
+
+module.exports = StyleSheet.create({
+    headText: {
+        color: theme.DARK_GREY_FONT_COLOR,
+        marginTop: 20,
+        fontSize: theme.REGULAR_FONT_SIZE,
+        fontFamily: theme.REGULAR_FONT_FAMILY,
+    },
+    ingredientText: {
+        color: theme.DARK_FONT_COLOR,
+        fontSize: theme.REGULAR_FONT_SIZE,
+        fontFamily: theme.REGULAR_FONT_FAMILY,
+    },
+});
