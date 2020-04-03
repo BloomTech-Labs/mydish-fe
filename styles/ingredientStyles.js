88d710af9a8ddd4813013c186acaf6f840980f0c
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-03 12:36:03 -0700

    adds container property

--- styles/ingredientStyles.js
+++ styles/ingredientStyles.js
@@ -2,6 +2,12 @@ import { StyleSheet } from "react-native";
 import theme from "./theme.style";
 
 module.exports = StyleSheet.create({
+    container: {
+        marginBottom: 12,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "flex-start",
+    },
     input: {
         height: theme.INPUT_HEIGHT,
         paddingHorizontal: 10,
