e3ffd887288caf6e477ac1cea019f7266cb7717c
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-03 14:01:06 -0700

    fixes step padding

--- components/Instruction.js
+++ components/Instruction.js
@@ -59,7 +59,7 @@ const Instruction = ({
                     style={{
                         maxWidth: "90%",
                         width: "90%",
-                        padding: 8,
+                        padding: 10,
                         borderWidth: theme.INPUT_BORDER_WIDTH,
                         borderColor: highlighted
                             ? theme.INPUT_BORDER_HIGHLIGHT_COLOR
