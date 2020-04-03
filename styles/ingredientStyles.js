546ca9a4969fdae3dde8a4adee5d843389d19fc2
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-04-03 12:34:21 -0700

    adds style properties

--- styles/ingredientStyles.js
+++ styles/ingredientStyles.js
@@ -1,4 +1,18 @@
 import { StyleSheet } from "react-native";
 import theme from "./theme.style";
 
-module.exports = StyleSheet.create({});
+module.exports = StyleSheet.create({
+    input: {
+        height: theme.INPUT_HEIGHT,
+        paddingHorizontal: 10,
+        borderWidth: theme.INPUT_BORDER_WIDTH,
+        borderColor: theme.INPUT_BORDER_COLOR,
+        borderRadius: theme.INPUT_BORDER_RADIUS,
+        textAlign: "left",
+        fontFamily: theme.INPUT_FONT_FAMILY,
+    },
+    quantity: { width: "20%" },
+    units: { width: "30%" },
+    name: { width: "40%", paddingBottom: 5 },
+    highlighted: { borderColor: theme.INPUT_BORDER_HIGHLIGHT_COLOR },
+});
