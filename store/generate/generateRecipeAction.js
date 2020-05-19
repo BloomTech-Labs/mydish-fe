cbfac5453ec203940623c9ee7d5b7f1e5b4b4b4d
Amos Rose <amosroseacct@gmail.com>
2020-05-19 13:04:24 -0600

    generate ingredients/instructions endpoints/actions updated

--- store/generate/generateRecipeAction.js
+++ store/generate/generateRecipeAction.js
@@ -1,48 +1,95 @@
 import axios from 'axios';
+import FormData from 'form-data';
 
 export const START_GENERATE_INGREDIENTS = 'START_GENERATE_INGREDIENTS';
 export const GENERATE_INGREDIENTS_SUCCESS = 'GENERATE_INGREDIENTS_SUCCESS';
 export const GENERATE_INGREDIENTS_FAILURE = 'GENERATE_INGREDIENTS_FAILURE';
-export const generateIngredients = (photo) => (dispatch) => {
+export const generateIngredients = (image) => async (dispatch) => {
   dispatch({ type: START_GENERATE_INGREDIENTS });
 
-  const photoPackage = {
-    img: photo,
+  const form = new FormData();
+  const imageData = {
+    uri: image,
+    type: 'image/jpeg',
+    name: image.split('/').pop(),
   };
+  form.append('image', imageData);
 
-  axios
-    .post(
-      'http://dishify2-env.eba-jpgptbu3.us-east-1.elasticbeanstalk.com/recipe_parser/ingredients/',
-      photo
-    )
-    .then((res) => {
-      console.log(res.data);
-      dispatch({ type: GENERATE_INGREDIENTS_SUCCESS, payload: res.data });
-    })
-    .catch((err) => {
-      console.log(err);
-      dispatch({ type: GENERATE_INGREDIENTS_FAILURE, payload: err });
+  try {
+    res = await axios({
+      method: 'post',
+      url:
+        'http://dishify1505-env.eba-b5yyyntm.us-east-1.elasticbeanstalk.com/recipe_parser/ingredients',
+      data: form,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'multipart/form-data',
+      },
     });
+    console.log(res.data);
+    dispatch({ type: GENERATE_INGREDIENTS_SUCCESS, payload: res.data });
+  } catch (err) {
+    console.log(err);
+    dispatch({ type: GENERATE_INGREDIENTS_FAILURE, payload: err });
+  }
+  // axios
+  //   .post(
+  //     'http://dishify1505-env.eba-b5yyyntm.us-east-1.elasticbeanstalk.com/recipe_parser/ingredients',
+
+  //   )
+  //   .then((res) => {
+  //     console.log(res.data);
+  //     dispatch({ type: GENERATE_INGREDIENTS_SUCCESS, payload: res.data });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     dispatch({ type: GENERATE_INGREDIENTS_FAILURE, payload: err });
+  //   });
 };
 export const START_GENERATE_INSTRUCTIONS = 'START_GENERATE_INSTRUCTIONS';
 export const GENERATE_INSTRUCTIONS_SUCCESS = 'GENERATE_INSTRUCTIONS_SUCCESS';
 export const GENERATE_INSTRUCTIONS_FAILURE = 'GENERATE_INSTRUCTIONS_FAILURE';
-export const generateInstructions = (photo) => (dispatch) => {
+export const generateInstructions = (image) => async (dispatch) => {
   dispatch({ type: START_GENERATE_INSTRUCTIONS });
 
-  axios
-    .post(
-      'http://dishify2-env.eba-jpgptbu3.us-east-1.elasticbeanstalk.com/recipe_parser/instructions/',
-      photo
-    )
-    .then((res) => {
-      console.log(res.data);
-      dispatch({ type: GENERATE_INSTRUCTIONS_SUCCESS, payload: res.data });
-    })
-    .catch((err) => {
-      console.log(err);
-      dispatch({ type: GENERATE_INSTRUCTIONS_FAILURE, payload: err });
+  const form = new FormData();
+  const imageData = {
+    uri: image,
+    type: 'image/jpeg',
+    name: image.split('/').pop(),
+  };
+  form.append('image', imageData);
+
+  try {
+    const res = await axios({
+      method: 'post',
+      url:
+        'http://dishify1505-env.eba-b5yyyntm.us-east-1.elasticbeanstalk.com/recipe_parser/instructions',
+      data: form,
+      headers: {
+        'content-type': `multipart/form-data`,
+      },
     });
+    console.log(res);
+    dispatch({ type: GENERATE_INGREDIENTS_SUCCESS, payload: res.data });
+  } catch (err) {
+    console.log(err);
+    dispatch({ type: GENERATE_INGREDIENTS_FAILURE, payload: err });
+  }
+
+  // axios
+  //   .post(
+  //     'http://dishify1505-env.eba-b5yyyntm.us-east-1.elasticbeanstalk.com/recipe_parser/instructions',
+  //     form
+  //   )
+  //   .then((res) => {
+  //     console.log(res.data);
+  //     dispatch({ type: GENERATE_INSTRUCTIONS_SUCCESS, payload: res.data });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     dispatch({ type: GENERATE_INSTRUCTIONS_FAILURE, payload: err });
+  //   });
 };
 
 export const START_GENERATE_GETTER = 'START_GENERATE_GETTER';
